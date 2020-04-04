ef1a7d49bf8935b8c6fee9375bf029b1151b4d6c
Andrea Fenderson <andrea_fenderson@ymail.com>
2020-04-04 18:48:40 -0500

    github username api works

--- Develop/utils/api.js
+++ Develop/utils/api.js
@@ -1,7 +1,17 @@
+const axios = require("axios");
+
 const api = {
   getUser(username) {
-
+    axios
+    .get(`https://api.github.com/users/AJFenderson`)
+    .then(response => console.log(response)) 
+    .catch(error => console.log(error))
   }
 };
 
+api.getUser();
+
 module.exports = api;
+
+
+// {headers: { Authorization: `token ${process.argv}`}}
