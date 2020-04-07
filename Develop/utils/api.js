df7e926c308327b72e922887d635898071e87796
Andrea Fenderson <andrea_fenderson@ymail.com>
2020-04-06 22:31:54 -0500

    changed to template literal, switched 'require dotenv...' to index.js file, token works again

--- Develop/utils/api.js
+++ Develop/utils/api.js
@@ -1,17 +1,21 @@
-const axios = require("axios");
+// require('dotenv').config()
+// const axios = require("axios");
 
-const api = {
-  getUser(username) {
-    axios
-    .get(`https://api.github.com/users/${username}`)
-    .then(response => console.log(response.data)) 
-    .catch(error => console.log(error))
-  }
-};
+// const api = {
+//   getUser(username) {
+//     axios
+//     .get(`https://api.github.com/users/${username}`, {
+//       headers: {"Authorization": `token ${process.env.GH_TOKEN}`}
+//     })
+//     .then(response => console.log(response.data)) 
+//     .catch(error => console.log(error))
+//   }
+// };
 
-api.getUser("AJFenderson");
+// api.getUser("acd37");
 
-module.exports = api;
+// module.exports = api;
 
 
 // {headers: { Authorization: `token ${process.argv}`}}
+// GH_TOKEN =a7f11a54e84f647bba88c0ef3e5d1dc1857e9b4d
--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+require('dotenv').config()
+const axios = require("axios");
+
+const api = {
+  getUser(username) {
+    axios
+    .get(`https://api.github.com/users/${username}`, {
+      headers: {"Authorization": `token ${process.env.GH_TOKEN}`}
+    })
+    .then(response => console.log(response.data)) 
+    .catch(error => console.log(error))
+  }
+};
+
+api.getUser("acd37");
+
+module.exports = api;
+
+
 const questions = [
 
 ];
